5a9a45e67098d8ce67ee51057e6269a72a48e970
Igor Usovich <usovigorok@gmail.com>
2018-09-05 08:55:13 +0200

    Layout

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ class App extends Component {
   render() {
 
     return (
-      <div className="App">
+      <div className="app">
         <RoomList
             roomId={this.state.roomId}
             subscribeToRoom={this.subscribeToRoom}
